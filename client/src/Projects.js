de058924815c30c72274da32bdc3c577d58e5675
JoshRinaberger <joshrinaberger@gmail.com>
2023-02-01 04:33:23 -0600

    Set up mysql and established connection with database. Made a simple test to select from a user table.

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -7,6 +7,5 @@ export default function Projects () {
             <Link to="/project/1">Project 1</Link>
             <Link to="/project/2">Project 2</Link>
         </div>
-        
     )
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,37 @@
 const express = require('express');
 const app = express();
 
+const mysql = require('mysql');
+
+// create mysql connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'daw-cache'
+});
+
+// connect DB
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Mysql connected.');
+    }
+});
+
+app.get('/user', (req, res) => {
+    let query = 'SELECT * from user';
+    db.query(query, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+            res.send(result);
+        }
+    });
+});
+
 app.get("/api", (req, res) => {
     res.json({"message" : "test"});
 });
