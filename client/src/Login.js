fa67d09fac5bc0f12d273ff606995fe69a41414c
JoshRinaberger <joshrinaberger@gmail.com>
2023-02-09 19:30:40 -0600

    Added email/password inputs, show/hide password, and submit buttons to the login page.

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,7 +1,44 @@
-
+import { useState } from "react";
 
 export default function Login () {
+    const [loginCredentials, setLoginCredentials] = useState({});
+    const [loginError, setLoginError] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+
+    function updateAccountName (e) {
+        let updatedLoginCredentials = loginCredentials;
+        updatedLoginCredentials.accountName = e.target.value;
+        setLoginCredentials(updatedLoginCredentials);
+    }
+
+    function updatePassword (e) {
+        let updatedLoginCredentials = loginCredentials;
+        updatedLoginCredentials.password = e.target.value;
+        setLoginCredentials(updatedLoginCredentials);
+    }
+
+    function updateShowPassword (e) {
+        setShowPassword(e.target.checked);
+    }
+
+    function login (e) {
+        console.log(loginCredentials);
+    }
+
     return (
-        <h1>Login</h1>
+        <div>
+            <h1>Login</h1>
+            <p className="error">{loginError}</p>
+            <p>Username/Email:</p>
+            <input type="text" onChange={updateAccountName}></input>
+            <p>Password:</p>
+            <input type={showPassword ? "text" : "password"} onChange={updatePassword}></input>
+            <p>Show Password:</p>
+            <input type="checkbox" onChange={updateShowPassword}></input>
+            <br></br>
+            <br></br>
+            <button onClick={login}>Login</button>
+        </div>
+
     )
 }
